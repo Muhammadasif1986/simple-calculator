fd9b940196c001565de7c02941f92329396259f1
MUHAMMAD ASIF <asif.abdulqadir19@outlook.com>
2024-03-21 23:44:56 +0500

    adding first calcuator

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+    {message: 'Enter First Number', type: 'number', name: 'firstNumber'},
+    {message: 'Enter Second Number', type: 'number', name: 'secondNumber'},
+    {
+        message: 'select one operator to perform operation',
+        type: 'list',
+        name: 'operator',
+        choices: ['Addition','subtraction','Mutiplication','Division'],
+
+    },
+])
+
+if(answers.operator==='Addition'){
+    console.log(answers.firstNumber + answers.secondNumber);}
+
+else if(answers.operator==='subtraction'){
+    console.log(answers.firstNumber - answers.secondNumber);}
+
+else if(answers.operator==='Mutiplication'){
+     console.log(answers.firstNumber * answers.secondNumber);}
+
+else if(answers.operator==='Division'){
+    console.log(answers.firstNumber / answers.secondNumber);}
+
+    else{
+        console.log('invalid input');
+    }
